cfddd7e2cbbd68281e939a87d6464ecdca3f44fa
GREENFONTS <godwillonyewuchii@gmail.com>
2022-07-17 23:07:28 +0100

    Updated Transaction Api

--- Bank Apis/Services/Mono/MonoAccountActions.cs
+++ Bank Apis/Services/Mono/MonoAccountActions.cs
@@ -2,6 +2,7 @@
 using Mono.Net.Sdk;
 using Mono.Net.Sdk.Models.Auth;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Runtime.InteropServices;
 
 namespace Bank_Apis.Services.Mono
@@ -95,8 +96,15 @@ namespace Bank_Apis.Services.Mono
             _client = _clientSetup.GetMonoClient(_dbclient, userId);
 
             var accountId = Utils.Monohelper.AccountId(_dbclient, branchId);
-            var response = await _client.Accounts.GetTransactions(accountId, null, null, null, 0, "debit", true);
-            return JsonConvert.SerializeObject(response.Data.Transactions);
+            var debitRes = await _client.Accounts.GetTransactions(accountId, null, null, null, 200, "debit", true);
+            var creditRes = await _client.Accounts.GetTransactions(accountId, null, null, null, 200, "credit", true);
+
+            var debitTransactions = JArray.Parse(JsonConvert.SerializeObject(debitRes.Data.Transactions));
+            var creditTransactions = JArray.Parse(JsonConvert.SerializeObject(creditRes.Data.Transactions));
+
+            var allTransactions = new JArray(debitTransactions.Union(creditTransactions));
+
+            return JsonConvert.SerializeObject(allTransactions);
         }
 
         public async Task<string> InstutitionsList()
